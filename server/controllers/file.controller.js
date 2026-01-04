45d24df11c507d7e37a0a9ec2a403dc003081849
just-bm <tbalancemurugan@gmail.com>
2026-01-05 01:21:30 +0530

    final revised backend.

--- server/controllers/file.controller.js
+++ server/controllers/file.controller.js
@@ -1,37 +1,3 @@
-// // Import the module
-
-// export const fileMetaDataExtraction = (req, res) => {
-//     try {
-//         console.log(req.files);
-//         if (!req.files || Object.keys(req.files).length === 0) {
-//             console.log("No files were uploaded.");
-//             return res.status(400).json({ message: 'No files were uploaded.' });
-//         }
-
-//         // Access the file (assuming key is 'file')
-//         const uploadedFile = req.files.file;
-
-//         return res.status(200).json({
-//             message: 'File uploaded successfully',
-//             file: {
-//                 name: uploadedFile.name,
-//                 size: uploadedFile.size,
-//                 mimetype: uploadedFile.mimetype,
-//                 md5: uploadedFile.md5
-//             }
-//         });
-
-//     } catch (err) {
-//         return res.status(500).json({
-//             message: err.message
-//         });
-//     }
-// };
-
-
-
-
-// file.controller.js
 import { v4 as uuidv4 } from "uuid";
 import csv from "csv-parser";
 import { Readable } from "stream";
